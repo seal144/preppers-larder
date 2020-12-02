ec8e1fd1b48c7f6d554796d6302743a32755be0c
konrad_fak <konrad.fak@10clouds.com>
2020-12-02 22:16:45 +0100

    Added larder component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
+import Larder from './larder/Larder.js'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Welcome to Preppers Larder!
       </header>
+
+      <div>
+        This is content
+        <Larder></Larder>
+      </div>
     </div>
   );
 }
---
+++ src/larder/Larder.js
@@ -0,0 +1,9 @@
+import './Larder.css';
+
+function Larder() {
+  return (
+    <p>This is Larder component</p>
+  );
+}
+
+export default Larder;
