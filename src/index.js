9365822739744515e8e0d92979f0d28b9034e8ed
konrad_fak <konrad.fak@10clouds.com>
2020-12-08 23:01:10 +0100

    Implemented ant desing library and added layout component

--- src/index.js
+++ src/index.js
@@ -2,38 +2,51 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
-import { Route, NavLink, BrowserRouter as Router } from 'react-router-dom'
+import { Route, Link, BrowserRouter as Router } from 'react-router-dom';
+import { Layout, Menu } from 'antd';
 
 import Larder from'./larder/Larder.js';
 import ShoppingList from './shopping-list/ShoppingList.js';
 
+const { Header, Content, Footer } = Layout;
+
 const routing = (
   <Router>
-    <div>
-      <ul>
-        <li>
-          <NavLink 
-            activeClassName="nav-active" 
-            to="/larder"
-          >
-            Larder
-          </NavLink>
-        </li>
-        <li>
-          <NavLink
-           activeClassName="nav-active" 
-           to="/shopping-list"
-          >
-            Shopping-list
-          </NavLink>
-        </li>
-      </ul>
-      <Route path="/larder" component={Larder} />
-      <Route path="/shopping-list" component={ShoppingList} />
-    </div>
+    <Layout className="layout">
+      <Header>
+        <Menu 
+          theme="dark" 
+          mode="horizontal" 
+          defaultSelectedKeys={['2']}
+        >
+          <Menu.Item key="1">
+            <Link to="/larder">Larder</Link>
+          </Menu.Item>
+          
+          <Menu.Item key="2">
+            <Link to="/shopping-list">Shopping-list</Link>
+          </Menu.Item>
+        </Menu>
+      </Header>
+
+      <Content style={{ padding: '0 50px' }}>
+        <Route 
+          path="/larder" 
+          component={Larder} 
+        />
+
+        <Route 
+          path="/shopping-list" 
+          component={ShoppingList} 
+        />
+      </Content>
+
+      <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
+    </Layout>
   </Router>
-)
-ReactDOM.render(routing, document.getElementById('root'))
+);
+
+ReactDOM.render(routing, document.getElementById('root'));
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
