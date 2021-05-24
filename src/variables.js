ec00e0fc34034d94a8a8b1a7c9fc8c732124c52e
seal144 <rafalfak8@gmail.com>
2021-05-24 21:02:48 +0200

    fixed responsifity for product form

--- src/variables.js
+++ src/variables.js
@@ -2,3 +2,4 @@ export const MAX_CHAR_INPUT = 20;
 export const MAX_CHAR_LONG_INPUT = 40;
 export const MIN_CHAR_PASSWORD = 3;
 export const MAX_CHAR_PASS_QUESTION = 90;
+export const MAX_CHAR_TEXT_INPUT = 300;
